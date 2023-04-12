83d09446490ecb09fde9cd54c29a5783851be790
Jhon Cabrera <jhoncabrera2050@gmail.com>
2023-04-12 16:17:10 -0500

    Curso_nube

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import { Router } from "express"
 const router = Router()
 router.get('/', (req,res) => res.render('index', {title: 'First Website with node js'}))
 router.get('/about', (req,res) => res.render('about', { title : 'About me'}))
+router.get('/clientes', (req,res) => res.render('clientes', { title : 'Clientes'}))
+router.get('/productos', (req,res) => res.render('productos', { title : 'Productos'}))
 router.get('/contact', (req,res) => res.render('contact', {title : 'COntact page'}))
 router.get('/Nuestros_servicios', (req,res) => res.render('servicios', {title : 'Servicios'}))
 router.get('/nosotros', (req,res) => res.render('nosotros', {title : 'nosotros'})) 
